d5af0a9baa0037987c4d4750efd6f29ff2e48ebb
Jagan Raajan <jaganraajan@Jagans-MacBook-Air.local>
2025-06-26 12:10:22 -0400

    edit home page

---
+++ src/components/board/projects-board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProjectsBoard: React.FC = () => {
+    return (
+        <div>
+            <h1>Projects Board</h1>
+            <p>Welcome to the Projects Board!</p>
+        </div>
+    );
+};
+
+export default ProjectsBoard;
